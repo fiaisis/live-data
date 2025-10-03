e39ee464ce94123f13a629d562f47910d58c92b8
Keiran Price <keiran.price@stfc.ac.uk>
2025-10-03 08:48:35 +0100

    Simplify workspaces.py

--- live_data_processor/workspaces.py
+++ live_data_processor/workspaces.py
@@ -1,10 +1,10 @@
+from pathlib import Path
+
 import numpy as np
+from mantid.api import mtd
+from mantid.simpleapi import GroupDetectors, LoadEmptyInstrument
 
 from schemas.compiled_schemas.RunStart import RunStart
-
-from mantid.api import mtd
-from mantid.kernel import DateAndTime
-from mantid.simpleapi import LoadEmptyInstrument, GroupDetectors
 
 
 def initialize_instrument_workspace(instrument: str, workspace_name: str, run_start: RunStart) -> None:
@@ -18,21 +18,8 @@ def initialize_instrument_workspace(instrument: str, workspace_name: str, run_st
     :param run_start: The RunStart message containing information about the new run.
     :return: None
     """
-    run_name = run_start.RunName()
-    run_name = run_name.decode("utf-8")
-    # LoadEmptyInstrument(InstrumentName=INSTRUMENT, OutputWorkspace=LIVE_WS_NAME, MakeEventWorkspace=True)
-    LoadEmptyInstrument(
-        OutputWorkspace="lives",
-        Filename="/Users/sham/miniconda3/envs/mantid/instrument/MERLIN_Definition.xml",
-        MakeEventWorkspace=True,
-    )
-    # Call instrument specific setup function
-    additional_setup_func = get_additional_workspace_setup(instrument)
-    additional_setup_func(run_start, workspace_name)
+    LoadEmptyInstrument(InstrumentName=instrument, OutputWorkspace=workspace_name, MakeEventWorkspace=True)
 
-
-def setup_merlin_workspace(run_start, workspace_name):
-    # merlin specific
     detector_ids = run_start.DetectorSpectrumMap().DetectorIdAsNumpy()
     spectrum = run_start.DetectorSpectrumMap().SpectrumAsNumpy()
     ws = mtd[workspace_name]
@@ -40,18 +27,9 @@ def setup_merlin_workspace(run_start, workspace_name):
     complete_spectrum = np.array(ws.getSpectrumNumbers())
     ids = [complete_spectrum[detector_ids[np.where(spectrum == sp)[0]]] for sp in np.unique(spectrum)]
     outstr = f"{len(ids)}\n" + "\n".join(
-        [f"{sp}\n{len(id)}\n" + " ".join([str(v) for v in id]) for sp, id in enumerate(ids)]
+        [f"{sp}\n{len(detector_id)}\n" + " ".join([str(v) for v in detector_id]) for sp, detector_id in enumerate(ids)]
     )
-    mapfile = "insttemp.map"
-    with open(mapfile, "w") as f:
+    mapfile = Path("insttemp.map")
+    with mapfile.open("w") as f:
         f.write(outstr)
     GroupDetectors(ws, mapfile, OutputWorkspace=workspace_name)
-
-
-def get_additional_workspace_setup(instrument: str):
-    match instrument.upper():
-        case "MERLIN":
-            return setup_merlin_workspace
-        case _:
-            # noop setup
-            return lambda *args, **kwargs: None
