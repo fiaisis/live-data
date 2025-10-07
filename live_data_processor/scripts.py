302bc95f99b973bc9bf11b45c3e62c13b4b89a7c
Keiran Price <keiran.price@stfc.ac.uk>
2025-10-07 08:53:47 +0100

    use lower case

--- live_data_processor/scripts.py
+++ live_data_processor/scripts.py
@@ -4,7 +4,7 @@ import os
 import requests
 
 logger = logging.getLogger(__name__)
-FIA_API_URL = os.environ.get("FIA_API_URL", "http://localhost:8000")
+FIA_API_URL = os.environ.get("FIA_API_URL", "https://dev.reduce.isis.cclrc.ac.uk/api")
 
 
 def get_script(instrument: str) -> str | None:
@@ -15,6 +15,10 @@ def get_script(instrument: str) -> str | None:
     """
 
     logger.info("Getting latest script")
-    response = requests.get(f"{FIA_API_URL}/live-data/{instrument}/script")
+    response = requests.get(f"{FIA_API_URL}/live-data/{instrument.lower()}/script")
+    print(response)
     return response.text if response.status_code == 200 else None
     # return """from mantid.simpleapi import *; SaveNexusProcessed(Filename="/output/output-lives.nxs", InputWorkspace="lives")"""  # noqa: E501
+
+
+print(get_script("MERLIN"))
