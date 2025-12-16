8a7bbffea8425a4203fcbdd30fd3d9e5e71fcbed
Keiran Price <keiran.price@stfc.ac.uk>
2025-12-16 15:19:14 +0000

    reformatting

--- live_data_processor/exceptions.py
+++ live_data_processor/exceptions.py
@@ -1,5 +1,6 @@
 class TopicIncompleteError(RuntimeError):
     """Raised when the Kafka topic is not yet complete"""
 
+
 class SampleLogError(RuntimeError):
     """Raised when there is an error with the sample log"""
