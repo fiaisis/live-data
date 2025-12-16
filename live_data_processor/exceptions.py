12b7396ef029079fdb80ec4248986e8f45e4631f
Keiran Price <keiran.price@stfc.ac.uk>
2025-12-16 14:35:21 +0000

    Add SampleLogError to exceptions module

--- live_data_processor/exceptions.py
+++ live_data_processor/exceptions.py
@@ -1,2 +1,5 @@
 class TopicIncompleteError(RuntimeError):
     """Raised when the Kafka topic is not yet complete"""
+
+class SampleLogError(RuntimeError):
+    """Raised when there is an error with the sample log"""
